1032d3d38d4c59d58f00cae7c23d5598001c4047
Altair-create <setyawandedi906@gmail.com>
2026-04-30 09:02:41 +0700

    Add files via upload

--- api/generate.js
+++ api/generate.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   // Izinkan akses dari browser
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
